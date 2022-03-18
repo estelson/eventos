9cf9988fdfa5eaaa7ffe8b40da701df4276192bb
Estelson Medeiros Pereira <estelson@gmail.com>
2022-03-17 23:41:26 -0300

    8. Deixando as mensagens da tela de login dinâmica

--- src/view/login/index.js
+++ src/view/login/index.js
@@ -11,12 +11,13 @@ function Login() {
      */
     const[email, setEmail] = useState();
     const[senha, setSenha] = useState();
+    const[msgTipo, setMsgTipo] = useState();
 
     function logar() {
         firebase.auth().signInWithEmailAndPassword(email, senha).then(resultado => {
-            alert("Usuário logado");
+            setMsgTipo("sucesso")
         }).catch(erro =>{
-            alert(erro);
+            setMsgTipo("erro");
         });
     }
 
@@ -34,9 +35,8 @@ function Login() {
                 <button onClick={logar} class="form-control btn btn-lg btn-primary btn-block btn-login" type="button">Logar</button>
 
                 <div className="msg-login text-white text-center my-5">
-                    <span><strong>WoW!</strong> Você está conectado! &#128526;</span>
-                    <br/>
-                    <span><strong>Ops!</strong> Verifique se a senha ou usuário estão corretos! &#128546;</span>
+                    {msgTipo == "sucesso" && <span><strong>WoW!</strong> Você está conectado! &#128526;</span>}
+                    {msgTipo == "erro" && <span><strong>Ops!</strong> Verifique se a senha ou usuário estão corretos! &#128546;</span>}
                 </div>
 
                 <div className="opcoes-login mt-5 text-center">
