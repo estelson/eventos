a536114e26b4868d96ae69abaea9d0f6b459f6d9
Estelson Medeiros Pereira <estelson@gmail.com>
2022-03-21 16:16:15 -0300

    Projeto final

--- src/view/evento-cadastro/index.js
+++ src/view/evento-cadastro/index.js
@@ -8,6 +8,40 @@ import Navbar from '../../components/navbar';
 function EventoCadastro() {
     const[msgTipo, setMsgTipo] = useState();
 
+    const[titulo, setTitulo] = useState();
+    const[tipo, setTipo] = useState();
+    const[detalhes, setDetalhes] = useState();
+    const[data, setData] = useState();
+    const[hora, setHora] = useState();
+    const[foto, setFoto] = useState();
+    const usuarioEmail = useSelector(state => state.usuarioEmail);
+
+    const storage = firebase.storage();
+    const db = firebase.firestore();
+
+    function cadastrar() {
+        setMsgTipo(null);
+
+        storage.ref(`imagens/${foto.name}`).put(foto).then(() => {
+            db.collection("eventos").add({
+                titulo: titulo,
+                tipo: tipo,
+                detalhes: detalhes,
+                data: data,
+                hora: hora,
+                usuario: usuarioEmail,
+                visualizacoes: 0,
+                foto: foto.name,
+                publico: 1,
+                criacao: new Date()
+            }).then(() => {
+                setMsgTipo("sucesso");
+            }).catch(erro => {
+                setMsgTipo("erro");
+            });
+        });
+    }
+
     return(
         <>
             <Navbar/>
@@ -20,12 +54,12 @@ function EventoCadastro() {
                 <form>
                     <div className="form-group">
                         <label>Título:</label>
-                        <input type="text" className="form-control"/>
+                        <input onChange={e => setTitulo(e.target.value)} type="text" className="form-control"/>
                     </div>
 
                     <div className="form-group">
                         <label>Tipo do evento:</label>
-                        <select className="form-control">
+                        <select onChange={e => setTipo(e.target.value)} className="form-control">
                             <option disabled selected value>-- Selecione um tipo --</option>
                             <option>Festa</option>
                             <option>Teatro</option>
@@ -36,27 +70,27 @@ function EventoCadastro() {
 
                     <div className="form-group">
                         <label>Descrição do evento:</label>
-                        <textarea className="form-control" rows="3"/>
+                        <textarea onChange={e => setDetalhes(e.target.value)} className="form-control" rows="3"/>
                     </div>
 
                     <div className="form-group row">
                         <div className='col-6'>
                             <label>Data:</label>
-                            <input type="date" className="form-control"/>
+                            <input onChange={e => setData(e.target.value)} type="date" className="form-control"/>
                         </div>
 
                         <div className='col-6'>
                             <label>Hora:</label>
-                            <input type="time" className="form-control"/>
+                            <input onChange={e => setHora(e.target.value)} type="time" className="form-control"/>
                         </div>
                     </div>
 
                     <div className="form-group">
                         <label>Upload da foto:</label>
-                        <input type="file" className="form-control"/>
+                        <input onChange={e => setFoto(e.target.files[0])} type="file" className="form-control"/>
                     </div>
 
-                    <button type="button" className="btn btn-lg btn-block mt-3 mb-5 form-control btn-cadastro">Publicar evento</button>
+                    <button type="button" onClick={cadastrar} className="btn btn-lg btn-block mt-3 mb-5 form-control btn-cadastro">Publicar evento</button>
                 </form>
 
                 <div className="msg-login text-center mt-2">
